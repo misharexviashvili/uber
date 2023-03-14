079266d6fafb7ee87278e0b90bd6b84fe6e46a71
Misha <misharexviashvili@yahoo.com>
2023-03-15 00:31:43 +0400

    Added some componenets, redux toolkit, Uber logo, tailwind

---
+++ components/NavOptions.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>NavOptions</Text>
+    </View>
+  )
+}
+
+export default NavOptions
+
+const styles = StyleSheet.create({})
