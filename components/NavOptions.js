a7506676b294c78280f2db4cd167c6fff9c5ae0c
Misha <misharexviashvili@yahoo.com>
2023-03-15 01:07:06 +0400

    added flatlist for drive and eat

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,13 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, TouchableOpacity } from "react-native";
+import { View, Image } from "react-native";
 
+const data = [
+  {
+    id: 123,
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: 456,
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
 const NavOptions = () => {
   return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
-  )
-}
+    <FlatList
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
 
-export default NavOptions
-
-const styles = StyleSheet.create({})
+export default NavOptions;
