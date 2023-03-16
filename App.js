5e95ea7b05a71626a4dcd4a295e28c4820d51462
Misha <misharexviashvili@yahoo.com>
2023-03-16 15:33:11 +0400

    styled ride and food images, added icons

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
@@ -14,12 +12,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
