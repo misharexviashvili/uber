9f7c984b93ae694f7d33cef5632707c0bd60556e
Misha <misharexviashvili@yahoo.com>
2023-03-16 20:56:29 +0400

    redux bug fixed reducer>>reducers

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,12 @@ import { View } from "react-native";
 import tw from "twrnc";
 import { Image } from "react-native";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 export default function HomeScreen() {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-3`}>
@@ -14,6 +19,38 @@ export default function HomeScreen() {
             resizeMode: "contain",
           }}
           source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where From?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          // debounce is timer in milisecs to start searching after stopping typing for this amount
+          debounce={200}
+          minLength={2}
+          returnKeyType={"search"}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          fetchDetails={true}
+          // Remove ad container in the end
+          enablePoweredByContainer={false}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
         />
         <NavOptions />
       </View>
