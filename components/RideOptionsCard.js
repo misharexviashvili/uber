2c9ad08eebf76eb6aa4819ca7f2270b0ad6a96ed
Misha <misharexviashvili@yahoo.com>
2023-03-17 18:29:20 +0400

    google distance matrix added

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,4 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 import { FlatList, Text, TouchableOpacity, View, Image } from "react-native";
 import { Icon } from "react-native-elements";
 import tw from "twrnc";
@@ -23,27 +24,33 @@ const data = [
   },
 ];
 const RideOptionsCard = () => {
+  const [selected, setSelected] = useState(null);
   const navigation = useNavigation();
   return (
     <View style={tw`bg-white flex-grow`}>
       <View>
         <TouchableOpacity
           onPress={() => navigation.goBack()}
-          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+          style={tw`absolute left-5 z-50 p-3 rounded-full`}
         >
           <Icon name="chevron-left" type="font-awesome" />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center py-2 text-xl`}>Select a Ride</Text>
       </View>
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
         renderItem={({ item: { id, title, multiplier, image }, item }) => (
-          <TouchableOpacity style={tw`flex-row justify-between items-center px-10`}>
+          <TouchableOpacity
+            style={tw`flex-row justify-between items-center px-8 ${
+              id === selected?.id ? "bg-gray-200" : null
+            }`}
+            onPress={() => setSelected(item)}
+          >
             <Image
               style={{
                 width: 100,
-                height: 100,
+                height: 90,
                 resizeMode: "contain",
               }}
               source={{ uri: image }}
@@ -56,6 +63,18 @@ const RideOptionsCard = () => {
           </TouchableOpacity>
         )}
       />
+      <View>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black py-3 mb-1 mx-2 ${
+            !selected ? "bg-gray-300" : null
+          }`}
+        >
+          <Text style={tw`text-center text-white text-xl `}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
