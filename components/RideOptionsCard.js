3b901a7e7e94615e3421063e9ed9faf799671776
Misha <misharexviashvili@yahoo.com>
2023-03-17 17:56:43 +0400

    added cars in select a ride place

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,13 +1,63 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import { useNavigation } from "@react-navigation/native";
+import { FlatList, Text, TouchableOpacity, View, Image } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "twrnc";
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "Uber X",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>pick a ride</Text>
+    <View style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="font-awesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity style={tw`flex-row justify-between items-center px-10`}>
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>$99</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
 
 export default RideOptionsCard;
-
-const styles = StyleSheet.create({});
